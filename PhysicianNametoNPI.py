1aeefce4c2f0c2fa9e926862e9f04ad07fcef574
ptran0604 <lien.l.tran@wustl.edu>
2022-07-09 00:07:16 -0700

    added address and primary taxonomy

--- PhysicianNametoNPI.py
+++ PhysicianNametoNPI.py
@@ -3,11 +3,12 @@ import requests
 import json
 import openpyxl
 class PhysicianName:
-  def __init__(self, first_name, last_name, state, row):
+  def __init__(self, first_name, last_name, state, row, second_last_name):
     self.first_name = first_name
     self.last_name = last_name
     self.state = state
     self.row = row
+    self.second_last_name = second_last_name
 # load excel with its path
 
 #load excel to get list of fname, lname, state
@@ -17,35 +18,54 @@ def get_physician_name_from_excel(file):
     # load excel with its path
     wrkbk = openpyxl.load_workbook(file)
 
-    sh = wrkbk.get_sheet_by_name('Triad HealthCare Network')
+    sh = wrkbk['Triad HealthCare Network']
 
     # iterate through excel and display data
-    for i in range(2, sh.max_row+1):
-        physician_name = PhysicianName(sh.cell(row=i, column = 1).value, sh.cell(row=i, column=2).value, "NC", row = i)
+    for i in range(1200, sh.max_row+1):
+        state = sh.cell(row = i, column = 4).value
+        second_last_name = sh.cell(row = i, column = 3).value
+        physician_name = PhysicianName(sh.cell(row=i, column = 1).value, sh.cell(row=i, column=2).value, state, i, second_last_name)
         physician_names.append(physician_name)
     get_npi_lists(physician_names, file, wrkbk, sh)
 #process each entry in the list
 def get_npi_lists(physician_names, file, wrkbk, sh):
     for physician_name in physician_names:
-        get_npi_from_name(physician_name, file, wrkbk, sh)
+    #edge case multiple last names
+        if(physician_name.second_last_name is None):
+            get_npi_from_name(physician_name, file, wrkbk, sh)
 #write the number back into excel
 
 def get_npi_from_name(physician_name, file, wrkbk, sh):
-    api_url = "https://npiregistry.cms.hhs.gov/api/?version=2.0&&pretty=true&state={st}&first_name={fname}&last_name={lname}".format(st = physician_name.state,fname=physician_name.first_name, lname = physician_name.last_name)
-    print(api_url)
+    api_url = "https://npiregistry.cms.hhs.gov/api/?version=2.0&&pretty=true&state={st}&first_name={fname}&last_name={lname}&use_first_name_alias=false".format(st = physician_name.state,fname=physician_name.first_name, lname = physician_name.last_name)
     response = requests.get(api_url)
-    if(response.json()["result_count"] > 0):
-        #edge case of multiple doctors in the same state with the same physician_name
-        #edge case if the doctor doesnt exist
-        #edge case multiple last names
+    result_count = response.json()["result_count"]
+    #edge case if the doctor doesnt exist
+    if(physician_name.row % 500 == 0):
+        print(physician_name.row)
+    if(result_count > 0):
         #cellref=sh.cell(row=physician_name.row, column=5)
         #cellref=response.json()["results"][0]["number"]
         cell = 'E' + str(physician_name.row)
-        sh[cell] = response.json()["results"][0]["number"]
-        print(response.json()["results"][0]["number"])
-    wrkbk.save("/Users/ptran/Downloads/ACOs.xlsx")
+        address_cell = "F" + str(physician_name.row)
+        taxonomy_cell = "G" + str(physician_name.row)
+        if(result_count == 1):
+            sh[cell] = response.json()["results"][0]["number"]
+            address = response.json()["results"][0]["addresses"][0]
+            sh[address_cell] = address["address_1"] + " " + address["address_2"] + ", " + address["city"] + ", " + address["state"] + ", " + address["postal_code"]
+            taxonomies_list = response.json()["results"][0]["taxonomies"]
+            for index in range(len(taxonomies_list)):
+                if(taxonomies_list[index]["primary"]):
+                    sh[taxonomy_cell] = taxonomies_list[index]["desc"]
+        else:
+            npi_list = ""
+            #edge case of multiple doctors in the same state with the same physician_name
+            for i in range(0, result_count):
+                npi_list += str(response.json()["results"][i]["number"]) + ","
+            sh[cell] = npi_list
 
-get_physician_name_from_excel("/Users/ptran/Downloads/ACOs.xlsx")
+    wrkbk.save("/Users/ptran2/Downloads/ACOs.xlsx")
+
+get_physician_name_from_excel("/Users/ptran2/Downloads/ACOs.xlsx")
 
 
 
